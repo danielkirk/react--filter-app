d6996ba64311306952c56cd30b9734a51e556430
imdanielkirk <imdanielkirk@gmail.com>
2020-01-20 20:34:20 -0800

    added components for list and card in order to show list of monsters.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,33 +1,25 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Dracula",
-          id: 1
-        },
-        {
-          name: "Frankenstein",
-          id: 2
-        },
-        {
-          name: "Zombie",
-          id: 3
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
   }
